084ddd0bb428e93a8f3253c7cdb7817a47cb3cd8
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-07-16 00:05:26 +0300

    unique id

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Modal, TextInput } from 'react-native';
 import { Audio } from 'expo-av';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   const [recording, setRecording] = useState();
   const [lastRecordingURI, setLastRecordingURI] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [fileName, setFileName] = useState('');
+  const [timer, setTimer] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (timerRunning) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 10);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [timerRunning]);
 
   async function startRecording() {
     try {
@@ -21,6 +37,8 @@ export default function App() {
       console.log('Starting recording..');
       const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
       setRecording(recording);
+      setTimer(0);
+      setTimerRunning(true);
       console.log('Recording started');
     } catch (err) {
       console.error('Failed to start recording', err);
@@ -30,6 +48,7 @@ export default function App() {
   async function stopRecording() {
     console.log('Stopping recording..');
     setRecording(undefined);
+    setTimerRunning(false);
     await recording.stopAndUnloadAsync();
     await Audio.setAudioModeAsync({
       allowsRecordingIOS: false,
@@ -53,22 +72,36 @@ export default function App() {
 
   function handleSave() {
     // Perform your save logic here using the fileName state variable
-    console.log('Saving audio file as:', fileName);
-
+    const uniqueID = generateUniqueID();
+    console.log('Saving audio file as:', fileName, 'Duration:', formatTime(timer), 'Unique ID:', uniqueID);
+    
+    
+    setTimer(0); // Reset the timer back to 0
     setFileName('');
     setModalVisible(false); // Close the modal after saving
   }
 
+  function formatTime(time) {
+    const minutes = Math.floor(time / 6000).toString().padStart(2, '0');
+    const seconds = Math.floor((time / 100) % 60).toString().padStart(2, '0');
+    const milliseconds = (time % 100).toString().padStart(2, '0');
+    return `${minutes}:${seconds}:${milliseconds}`;
+  }
+
+  function generateUniqueID() {
+    const now = new Date();
+    const dateString = now.toISOString().replace(/[-:.TZ]/g, '');
+    return `rec${dateString}`;
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.timerContainer}>
+        <Text style={styles.timer}>{formatTime(timer)}</Text>
+      </View>
       <Button
-        title={recording ? <Ionicons name="stop-circle-outline" size={48} color="black" />: <Ionicons name="mic" size={48} color="white" />}
+        title={recording ? <Ionicons name="stop-circle-outline" size={48} color="white" />: <Ionicons name="mic" size={48} color="white" />}
         onPress={recording ? stopRecording : startRecording}
-      />
-      <Button
-        title="Play Last Recording"
-        onPress={playLastRecording}
-        disabled={!lastRecordingURI} // Disable the button if there is no last recording URI
       />
 
       <Modal
@@ -97,6 +130,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 10,
+  },
+  timerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timer: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'blue',
   },
   modalContainer: {
     flex: 1,
--- models/recordings.models.js
+++ models/recordings.models.js
@@ -4,7 +4,8 @@ const recordSchema = new mongoose.Schema({
     rec_id:{ type: String, required: true, unique: true},
     user_id:{ type: String, required: true},
     audio_name:{ type: String, required: true},
-    audio_file: { type: Buffer, required: true }
+    audio_file: { type: Buffer, required: true },
+    duration:{ type: String, required: true},
 })
 
 module.exports = mongoose.model('Recordings',recordSchema);
